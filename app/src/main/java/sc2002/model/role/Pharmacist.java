19bccc00ce8811b5454d0fa4a558ed2934cac542
flare <c230139@e.ntu.edu.sg>
2024-11-15 19:37:00 +0800

    refactor(role): extend Pharmacist from Staff
    Introduce inheritance from Staff to Pharmacist,
    passing required attributes to the superclass constructor.

--- app/src/main/java/sc2002/model/role/Pharmacist.java
+++ app/src/main/java/sc2002/model/role/Pharmacist.java
@@ -2,5 +2,8 @@ package sc2002.model.role;
 
 import java.io.Serializable;
 
-public class Pharmacist implements Serializable {
+public class Pharmacist extends Staff implements Serializable {
+    public Pharmacist(String id, String username, String firstName, String lastName, String password, String email, String phoneNumber, UserRole role) {
+        super(id, username, firstName, lastName, password, email, phoneNumber, role);
+    }
 }
