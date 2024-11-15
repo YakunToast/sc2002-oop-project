8aab15367937c4453667f8e392df8dcddb233be7
flare <c230139@e.ntu.edu.sg>
2024-11-15 19:37:22 +0800

    fixup! refactor(role): extend Pharmacist from Staff

--- app/src/main/java/sc2002/model/role/Pharmacist.java
+++ app/src/main/java/sc2002/model/role/Pharmacist.java
@@ -1,8 +1,6 @@
 package sc2002.model.role;
 
-import java.io.Serializable;
-
-public class Pharmacist extends Staff implements Serializable {
+public class Pharmacist extends Staff {
     public Pharmacist(String id, String username, String firstName, String lastName, String password, String email, String phoneNumber, UserRole role) {
         super(id, username, firstName, lastName, password, email, phoneNumber, role);
     }
